d875c04ee8972de23d7d35efcdc7120766506e52
lotcarnage <lotcarnage@gmail.com>
2023-04-05 03:08:20 +0900

    [update] プロジェクトを作成してタスクをぶら下げられる機能を追加。

--- code/main.py
+++ code/main.py
@@ -8,7 +8,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = flask_sqlalchemy.SQLAlchemy(app)
 
 User = models.LoadModel_User(db)
+Project = models.LoadModel_Project(db)
 Task = models.LoadModel_Task(db)
+WorkSchedule = models.LoadModel_WorkSchedule(db)
 
 
 def _try_login(user_id: str, password: str) -> bool:
@@ -42,25 +44,45 @@ def home():
     print(session_id)
     if session_id is None:
         return flask.redirect('/')
+    projects = Project.query.all()
     tasks = Task.query.all()
-    return flask.render_template('home.html', tasks=tasks)
+    return flask.render_template('home.html', projects=projects, tasks=tasks)
+
+
+@app.route('/projects', methods=['get'])
+def projects():
+    projects = Project.query.all()
+    for project in projects:
+        project.task_count = len(Task.query.filter_by(parent_project_id=project.id).all())
+    return flask.render_template('projects.html', projects=projects)
+
+
+@app.route('/addproject', methods=['post'])
+def addproject():
+    project_name = flask.request.form.get('project_name')
+    new_project = Project(name=project_name, create_at=datetime.datetime.now())
+    db.session.add(new_project)
+    db.session.commit()
+    return flask.redirect('/projects')
+
+
+@app.route('/projectedit', methods=['get'])
+def projectedit():
+    project_id = flask.request.args.get('id', '')
+    project = Project.query.filter_by(id=project_id).first()
+    tasks = Task.query.filter_by(parent_project_id=project_id).all()
+    return flask.render_template('projectedit.html', tasks=tasks, project=project)
 
 
 @app.route('/addtask', methods=['post'])
 def addtask():
-
+    project_id = flask.request.form.get('project_id')
     subject = flask.request.form.get('subject')
     description = flask.request.form.get('description')
-    new_task = Task(subject=subject, description=description)
+    new_task = Task(subject=subject, description=description, parent_project_id=project_id)
     db.session.add(new_task)
     db.session.commit()
-    tasks = Task.query.all()
-    response_html = flask.render_template('home.html', tasks=tasks)
-    response = flask.make_response(response_html)
-    max_age = 10  # 10 sec
-    expires = int(datetime.datetime.now().timestamp()) + max_age
-    response.set_cookie('session_id', value='hogehoge', max_age=max_age, expires=expires, path='/', secure=None, httponly=False)
-    return flask.redirect('/home', Response=response)
+    return flask.redirect(f'/projectedit?id={project_id}')
 
 
 def _init_db():
@@ -73,7 +95,8 @@ def _init_db():
 
 
 if __name__ == "__main__":
-    if False:
+    import sys
+    if 1 < len(sys.argv):
         with app.app_context():
             db.create_all()
             _init_db()
--- code/models.py
+++ code/models.py
@@ -1,20 +1,62 @@
 import flask_sqlalchemy
+import enum
+
+
+class ProjectState(enum.Enum):
+    PREPARING = 0
+    DOING = 1
+    COMPLETED = 2
+    LOST = 3
+
+
+class TaskState(enum.Enum):
+    WAITING = 0
+    WORKING = 1
+    WAITING_REVIEW = 2
+    COMPLETED = 3
+    CANCELED = 4
 
 
 def LoadModel_User(db: flask_sqlalchemy.SQLAlchemy):
     class User(db.Model):
         __tablename__ = 'users'
-        username = db.Column(db.String(50), primary_key=True, unique=True)
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        username = db.Column(db.String(50), unique=True)
         password = db.Column(db.String(50))
         create_at = db.Column(db.DateTime)
     return User
+
+
+def LoadModel_Project(db: flask_sqlalchemy.SQLAlchemy):
+    class Project(db.Model):
+        __tablename__ = 'projects'
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        name = db.Column(db.String(50), unique=True)
+        sales_budget = db.Column(db.Integer)
+        state = db.Column(db.Enum(ProjectState))
+        create_at = db.Column(db.DateTime)
+    return Project
 
 
 def LoadModel_Task(db: flask_sqlalchemy.SQLAlchemy):
     class Task(db.Model):
         __tablename__ = 'tasks'
         id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        asigned_user_id = db.Column(db.Integer)
+        parent_project_id = db.Column(db.Integer)
         subject = db.Column(db.String(50))
         description = db.Column(db.String(50))
         create_at = db.Column(db.DateTime)
+        estimation_working_minute = db.Column(db.Integer)
     return Task
+
+
+def LoadModel_WorkSchedule(db: flask_sqlalchemy.SQLAlchemy):
+    class WorkSchedule(db.Model):
+        __tablename__ = 'workschedules'
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        asigned_user_id = db.Column(db.Integer)
+        parent_task_id = db.Column(db.Integer)
+        start_at = db.Column(db.DateTime)
+        working_minute = db.Column(db.Integer)
+    return WorkSchedule
