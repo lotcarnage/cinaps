f319229bcac099342c98546c099a977eeb45759a
lotcarnage <lotcarnage@gmail.com>
2023-04-06 02:18:30 +0900

    [fix] リダイレクト方法が間違っていてcoockieをクライアントへ返せていなかった不具合を修正。

--- code/main.py
+++ code/main.py
@@ -29,12 +29,11 @@ def login():
     password = flask.request.form.get('password')
     if _try_login(user_id, password) is False:
         return flask.redirect('/')
-    response_html = flask.render_template('home.html')
-    response = flask.make_response(response_html)
-    max_age = 10  # 10 sec
+    response = flask.make_response(flask.redirect(flask.url_for('home')))
+    max_age = 60  # 10 sec
     expires = int(datetime.datetime.now().timestamp()) + max_age
-    response.set_cookie('session_id', value='hogehoge', max_age=max_age, expires=expires, path='/', secure=None, httponly=False)
-    return flask.redirect('/home', Response=response)
+    response.set_cookie('session_id', value='hogehoge', max_age=max_age, expires=expires, secure=None, httponly=False)
+    return response
 
 
 @app.route('/home', methods=['get'])
